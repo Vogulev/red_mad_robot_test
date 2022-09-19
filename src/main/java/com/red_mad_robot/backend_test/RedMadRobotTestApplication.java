c81c3205e585c6b5c082e8a94b656ddf57a2cdfd
Artem.Vogulev <Artem.Vogulev@russianpost.ru>
2022-09-19 22:11:16 +0300

    Initial commit

---
+++ src/main/java/com/red_mad_robot/backend_test/RedMadRobotTestApplication.java
@@ -0,0 +1,13 @@
+package com.red_mad_robot.backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedMadRobotTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedMadRobotTestApplication.class, args);
+    }
+
+}
